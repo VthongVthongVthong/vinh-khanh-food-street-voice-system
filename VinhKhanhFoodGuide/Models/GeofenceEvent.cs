c63ffa7fb1ac06f3f24bc78c2bc052105b37459e
Võ Hoàng Bảo <vohoangbao082005@gmail.com>
2026-03-16 16:12:59 +0700

    Delete VinhKhanhFoodGuide directory

--- VinhKhanhFoodGuide/Models/GeofenceEvent.cs
+++
@@ -1,9 +0,0 @@
-namespace VinhKhanhFoodGuide.Models;
-
-public class GeofenceEvent
-{
-    public int PoiId { get; set; }
-    public string PoiName { get; set; }
-    public DateTime TriggerTime { get; set; }
-    public double Distance { get; set; } // meters
-}
--- VinhKhanhFoodGuide/Models/LocationData.cs
+++
@@ -1,9 +0,0 @@
-namespace VinhKhanhFoodGuide.Models;
-
-public class LocationData
-{
-    public double Latitude { get; set; }
-    public double Longitude { get; set; }
-    public double Speed { get; set; } // m/s
-    public DateTime Timestamp { get; set; }
-}
